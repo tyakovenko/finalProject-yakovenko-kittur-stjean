b01dfead025943c398db6d4a05230235358e6289
m-saint <stjeanmyles@gmail.com>
2024-02-28 18:50:13 -0500

    experiment stuff

---
+++ donutExperiment/json.js
@@ -0,0 +1,297 @@
+export const json = {
+    "title": "Donut Chart Experiment",
+    "description": "Please answer the following questions to the best of your ability. Remember not to spend too much time on each question. Instead, go with your initial \"eyeballed\" guess. \n\nNote: The following charts each add up to 100%",
+    "logoPosition": "right",
+    "pages": [
+     {
+      "name": "page1",
+      "elements": [
+       {
+        "type": "image",
+        "name": "chart1",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=b0cf83e3-a075-4a16-8315-52600ac7a08d",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question1",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart2",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=3b123d68-1bf1-470d-b9b1-96b6c1ddbe11",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question2",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart3",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=20da41a5-4b1e-41ee-857d-3392541e442d",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question3",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart4",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=0828c109-902f-47bd-b52f-b5792731537b",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question4",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart5",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=3bdc90cf-ee40-480c-aab3-a3aafa39366a",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question5",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart6",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=dacfd883-1a07-4082-aa48-2a237e83b3ce",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question6",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart7",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=d13e8f8a-fa80-4802-9fd6-8756fea91d4f",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question7",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart8",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=9e97fcea-9f14-4e82-9d02-a7c8c262069d",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question8",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart9",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=9acd0044-0c4b-4322-94b5-70e0a06a0f25",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question9",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart10",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=bb331511-079d-435d-bd06-bbee755335e7",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question10",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       }
+      ]
+     },
+     {
+      "name": "page2",
+      "elements": [
+       {
+        "type": "image",
+        "name": "chart11",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=7794182b-0ef9-401b-84db-d170382cc8cb",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question11",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart12",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=3f4303c1-a4be-4f1c-a1f9-f929bf3a6a2e",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question12",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart13",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=ca8e83fd-fb57-4e01-9d3d-389995303c99",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question13",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart14",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=50519c00-515e-4104-adc2-dfe02c5108b8",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question14",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart15",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=4ca167e8-68ab-485d-a122-0688685638b1",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question15",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart16",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=9106111b-7a48-4a4b-92f2-66de9270dc10",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question16",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart17",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=8e8ca9a6-5ff8-4123-be55-b4db330eb4c2",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question17",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart18",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=614b79d8-91fd-468e-bfa9-c0aba128240e",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question18",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart19",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=84495568-3a8e-4c35-a9f0-cd3e316f58c9",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question19",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart20",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=55796757-9cbe-49e5-8a4a-df9cff38949d",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question20",
+        "title": "What is the difference between the RED sections?",
+        "isRequired": true
+       }
+      ]
+     }
+    ]
+   }
