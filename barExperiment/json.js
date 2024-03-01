335e69fc781130c08ee7af0e3eeb69274d7b26bb
m-saint <stjeanmyles@gmail.com>
2024-03-01 12:13:24 -0500

    experiments

---
+++ barExperiment/json.js
@@ -0,0 +1,297 @@
+export const json = {
+    "title": "Bar Chart Experiment",
+    "description": "description",
+    "logoPosition": "right",
+    "pages": [
+     {
+      "name": "page1",
+      "elements": [
+       {
+        "type": "image",
+        "name": "chart1",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=f1e1c124-51ff-436e-8bb8-586883a603d9",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question1",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart2",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=d0e1bee3-ce79-4696-b2a4-5224e33ab3b4",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question2",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart3",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=90cc8c8a-1774-42f1-bee5-56ed22cba30e",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question3",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart4",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=3e9d8a33-ca37-43fd-8580-d2ebe1d12645",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question4",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart5",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=14516bba-eb8f-4241-8432-e921d3d6457f",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question5",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart6",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=a01e9435-4c0c-4c5d-aa61-897a7aaf0cf6",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question6",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart7",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=f4d94879-498f-4841-8def-544d646ea995",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question7",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart8",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=c0b70f26-0ec0-41cb-82c8-446177b52086",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question8",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart9",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=0dc12d7e-7f36-4dd5-bef0-85e792823499",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question9",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart10",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=3364d6b1-e607-496b-bf33-c010bc7226b4",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question10",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       }
+      ]
+     },
+     {
+      "name": "page2",
+      "elements": [
+       {
+        "type": "image",
+        "name": "chart11",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=bdf93e00-274f-4878-b9d6-11eaba9dd86c",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question11",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart12",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=b48e1479-0cb2-4472-8683-3c48075c2687",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question12",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart13",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=8a4f892e-d3c0-497b-b0f6-4884da486f53",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question13",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart14",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=9519ea0e-8211-4b7e-bb45-d851dd3b653c",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question14",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart15",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=f18f9777-c0a2-4f68-8d20-c9c295f27d9e",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question15",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart16",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=feb63548-8f67-4155-a742-2bbc2801e1d1",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question16",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart17",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=f43c4fa4-33c5-4005-abca-2e6e01c69c89",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question17",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart18",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=8f518dac-db61-4131-a730-fac0a5a91786",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question18",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart19",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=4fd76d43-f6a8-40c5-b629-302abbf7f9db",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question19",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart20",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=77916407-d161-4627-af9f-a8baacaf610b",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question20",
+        "title": "What is the difference between the bars labeled with red dots?",
+        "isRequired": true
+       }
+      ]
+     }
+    ]
+   }
---
+++ radarExperiment/json.js
@@ -0,0 +1,297 @@
+export const json = {
+    "title": "Radar Chart Experiment",
+    "description": "Please answer the following questions to the best of your ability. Remember not to spend too much time on each question. Instead, go with your initial \"eyeballed\" guess.",
+    "logoPosition": "right",
+    "pages": [
+     {
+      "name": "page1",
+      "elements": [
+       {
+        "type": "image",
+        "name": "chart1",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=53e224d1-9631-4d07-99b2-2c352b59c1e7",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question1",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart2",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=cca90cb8-41f4-4609-bf80-d80b3cef4bd4",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question2",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart3",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=1c404c2b-5d75-4143-a45a-42fcd83483fb",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question3",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart4",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=f09e148e-130a-4937-8ece-781cde16799b",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question4",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart5",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=89c2f354-c437-4b28-9560-0849439a3efa",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question5",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart6",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=1166af37-d11b-4765-83a9-81efe25d9590",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question6",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart7",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=bef9739b-a7e8-427c-80a2-29a60821ab26",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question7",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart8",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=2c34fc0b-b45a-433d-9443-4ca5841cb6c8",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question8",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart9",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=a2b46745-1c1b-4be6-8820-17b71a81d328",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question9",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart10",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=08edff5e-4098-4ecb-8002-3b2abd086d95",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question10",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       }
+      ]
+     },
+     {
+      "name": "page2",
+      "elements": [
+       {
+        "type": "image",
+        "name": "chart11",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=906c32ed-67a1-4d5b-b330-0df3f9729752",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question11",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart12",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=884996d4-9d18-4e51-a0e9-637f761d3e3e",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question12",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart13",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=bd3daf40-9fd1-497f-b19c-19b154126d33",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question13",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart14",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=54b6aedb-8c55-49f8-83d1-6e0482a0e547",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question14",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart15",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=4b8c5201-270f-4cbd-8f6b-19b25c759072",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question15",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart16",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=96d8258c-39d2-4966-92b8-ed2f11d365ca",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question16",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart17",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=7010b500-3d36-4c0e-998c-f932fd77d025",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question17",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart18",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=0c592df3-c8cb-4730-9b14-02c52bed3a4c",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question18",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart19",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=c763386f-0516-4ccf-b4a5-9cf257430ad2",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question19",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       },
+       {
+        "type": "image",
+        "name": "chart20",
+        "imageLink": "https://api.surveyjs.io/private/Surveys/files?name=be37ec0d-6634-4b4a-89b2-3a2deb4c0c2e",
+        "imageFit": "cover",
+        "imageHeight": "auto",
+        "imageWidth": "100%"
+       },
+       {
+        "type": "text",
+        "name": "question20",
+        "title": "What is the difference between the sections labeled with red dots?",
+        "isRequired": true
+       }
+      ]
+     }
+    ]
+   }
